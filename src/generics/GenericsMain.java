59cd4c363b2f04a0a9d9009289212ff4bd3db002
mfarsikov <farsikovmax@gmail.com>
2016-05-13 21:05:20 +0300

    generics (final)

--- src/generics/GenericsMain.java
+++ src/generics/GenericsMain.java
@@ -6,7 +6,9 @@ import OOP.inheritance.hierarchy.Person;
 import multithreading.bank.Bank;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by MBCNEWMAIN on 06.05.2016.
@@ -19,5 +21,72 @@ public class GenericsMain {
 
         Employee employee = employees.get(0);
         employee.getPosition();
+
+        Manager manager = new Manager();
+        manager.setName("Vasya");
+        Employee employee1 = new Employee();
+
+        employee1 = manager;
+
+        employee1.greeting();
+
+        ArrayList<Manager> managers = new ArrayList<>();
+        employees.add(new Employee());
+        employee = employees.get(0);
+
+        managers.add(new Manager());
+        Manager manager1 = managers.get(0);
+
+        employee = manager;
+        //employees = managers;
+
+        Manager myManager = managers.get(0);
+        employees.add(new Employee());
+
+        employees.add(manager);
+
+        fire(employees);
+        fire(managers);
+
+        Pair<Person, Person> personPair = new Pair<>(new Person(), new Person());
+        Person first = personPair.getFirst();
+
+        Pair<String, String> stringPair = new Pair<>("sdf", "ASDF");
+        String second = stringPair.getSecond();
+
+        Map<Person, String> personStringMap = new HashMap<>();
+        personStringMap.put(manager, "some string");
+
+        copy(employees, new ArrayList<>());
+
+        List<Employee> employees1 = copy(managers);
+    }
+
+    /*public static void fire(List<Manager> employees){
+        for(Manager employee : employees){
+            System.out.println(employee.getName() + " is fired");
+        }
+    }*/
+
+    public static void fire(List<? extends Employee> employees){
+        for(Employee employee : employees){
+            System.out.println(employee.getName() + " is fired");
+        }
+        //employees.add(new Employee());
+    }
+
+    public static void addEmployee(List<? super Employee> employees){
+        employees.add(new Employee());
+        //Employee employee = employees.get(0);
+    }
+
+    public static <T> void copy(List<? extends T> source, List<T> target){
+        target.addAll(source);
+    }
+
+    public static <T> List<T> copy(List<? extends T> source){
+        List<T> target = new ArrayList<>();
+        target.addAll(source);
+        return target;
     }
 }
---
+++ src/generics/PersonComparator.java
@@ -0,0 +1,16 @@
+package generics;
+
+import OOP.inheritance.hierarchy.Person;
+
+import java.util.Comparator;
+
+/**
+ * Created by MBCNEWMAIN on 13.05.2016.
+ */
+public class PersonComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+
+        return 0;
+    }
+}
